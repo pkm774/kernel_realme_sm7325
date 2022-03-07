c1006b91be199e74eb20f986a6e988b1afc1c49f
pkm774 <mprabhat774@gmail.com>
2022-03-07 18:33:23 +0530

    RMX3360: Update build script
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- build.sh
+++ build.sh
@@ -2,26 +2,44 @@
 TOP=$(realpath ../)
 
 # export aosp gcc toolchains path
-export PATH="$TOP/clang/host/linux-x86/clang-r428724/bin:$PATH"
-export LD_LIBRARY_PATH="$TOP/clang/host/linux-x86/clang-r428724/lib64:$LD_LIBRARY_PATH"
-export PATH="$TOP/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:$PATH"
-export MAKE_PATH=${TOP}/build-tools/linux-x86/bin/
-
-export KERNEL_ARCH=arm64
-export ARCH=${KERNEL_ARCH}
-export DEFCONFIG=lahaina_defconfig
-
-export CROSS_COMPILE=${TOP}/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export CLANG_TRIPLE=aarch64-linux-gnu-
 
 
-export TARGET_INCLUDES=${TARGET_KERNEL_MAKE_CFLAGS}
-export TARGET_LINCLUDES=${TARGET_KERNEL_MAKE_LDFLAGS}
+PATH="$TOP/tools/build-tools/linux-x86/bin:$PATH"
+PATH="$TOP/tools/build-tools/path/linux-x86:$PATH"
+PATH="$TOP/tools/gas/linux-x86:$PATH"
+PATH="$TOP/tools/misc/linux-x86/lz4:$PATH"
+PATH="$TOP/tools/misc/linux-x86/dtc:$PATH"
+PATH="$TOP/tools/misc/linux-x86/libufdt:$PATH"
+PATH="$TOP/tools/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:$PATH"
+PATH="$TOP/tools/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin:$PATH"
+PATH="$TOP/tools/clang/host/linux-x86/clang-r428724/bin:$PATH"
+export LD_LIBRARY_PATH="$TOP/tools/clang/host/linux-x86/clang-r428724/lib64:$LD_LIBRARY_PATH"
+export DTC_EXT="$TOP/tools/misc/linux-x86/dtc/dtc"
+export DTC_OVERLAY_TEST_EXT="$TOP/tools/misc/linux-x86/libufdt/ufdt_apply_overlay"
+MAKE_PATH=${TOP}/tools/build-tools/linux-x86/bin/
 
-TARGET_KERNEL_MAKE_ENV+="HOSTCC=${TOP}/clang/host/linux-x86/clang-r428724/bin/clang "
-TARGET_KERNEL_MAKE_ENV+="CC=${TOP}/clang/host/linux-x86/clang-r428724/bin/clang "
-TARGET_KERNEL_MAKE_ENV+="LLVM=${TOP}/clang/host/linux-x86/clang-r428724/bin/clang"
+KERNEL_ARCH=arm64
+ARCH=${KERNEL_ARCH}
+DEFCONFIG=lahaina_defconfig
 
-${MAKE_PATH}make O=out ${TARGET_KERNEL_MAKE_ENV} ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CFLAGS="${TARGET_INCLUDES}" LDFLAGS="${TARGET_LINCLUDES}" ${DEFCONFIG}
+CLANG_TRIPLE=aarch64-linux-gnu-
+CROSS_COMPILE=aarch64-linux-android-
+CROSS_COMPILE_COMPAT=arm-linux-androideabi-
 
-${MAKE_PATH}make O=out ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CFLAGS="${TARGET_INCLUDES}" LDFLAGS="${TARGET_LINCLUDES}" ${TARGET_KERNEL_MAKE_ENV} -j$(nproc)
+TARGET_KERNEL_MAKE_ENV+="HOSTCC=${TOP}/tools/clang/host/linux-x86/clang-r428724/bin/clang "
+TARGET_KERNEL_MAKE_ENV+="HOSTCXX=${TOP}/tools/clang/host/linux-x86/clang-r428724/bin/clang++ "
+TARGET_KERNEL_MAKE_ENV+="HOSTLD=${TOP}/tools/clang/host/linux-x86/clang-r428724/bin/ld.lld "
+TARGET_KERNEL_MAKE_ENV+="CC=${TOP}/tools/clang/host/linux-x86/clang-r428724/bin/clang "
+TARGET_KERNEL_MAKE_ENV+="LLVM=${TOP}/tools/clang/host/linux-x86/clang-r428724/bin/clang "
+TARGET_KERNEL_MAKE_ENV+="LD=${TOP}/tools/clang/host/linux-x86/clang-r428724/bin/ld.lld "
+TARGET_KERNEL_MAKE_ENV+="AR=${TOP}/tools/clang/host/linux-x86/clang-r428724/bin/llvm-ar "
+TARGET_KERNEL_MAKE_ENV+="NM=${TOP}/tools/clang/host/linux-x86/clang-r428724/bin/llvm-nm "
+TARGET_KERNEL_MAKE_ENV+="OBJCOPY=${TOP}/tools/clang/host/linux-x86/clang-r428724/bin/llvm-objcopy "
+TARGET_KERNEL_MAKE_ENV+="OBJDUMP=${TOP}/tools/clang/host/linux-x86/clang-r428724/bin/llvm-objdump "
+TARGET_KERNEL_MAKE_ENV+="OBJSIZE=${TOP}/tools/clang/host/linux-x86/clang-r428724/bin/llvm-size "
+TARGET_KERNEL_MAKE_ENV+="READELF=${TOP}/tools/clang/host/linux-x86/clang-r428724/bin/llvm-readelf "
+TARGET_KERNEL_MAKE_ENV+="STRIP=${TOP}/tools/clang/host/linux-x86/clang-r428724/bin/llvm-strip "
+
+${MAKE_PATH}make O=out ${TARGET_KERNEL_MAKE_ENV} ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CROSS_COMPILE_COMPAT=${CROSS_COMPILE_COMPAT} CLANG_TRIPLE=${CLANG_TRIPLE} ${DEFCONFIG}
+
+${MAKE_PATH}make O=out ${TARGET_KERNEL_MAKE_ENV} ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CROSS_COMPILE_COMPAT=${CROSS_COMPILE_COMPAT} CLANG_TRIPLE=${CLANG_TRIPLE} -j$(nproc)
